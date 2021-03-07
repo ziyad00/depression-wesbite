aa2975b286c360ff1c1135c4d139f7e72e2eaf46
ziyad00 <ziyad.alotaibe@gmail.com>
2021-03-07 23:24:03 +0300

    dd

--- invorter/settings.py
+++ invorter/settings.py
@@ -126,6 +126,6 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+#import dj_database_url 
+#prod_db  =  dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(prod_db)
--- invorterapp/migrations/0002_time.py
+++ invorterapp/migrations/0002_time.py
@@ -5,21 +5,16 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
-
     dependencies = [
+        ('invorterapp', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Message',
+            name='Time',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('text', models.TextField()),
-                ('created', models.DateTimeField(auto_now_add=True)),
+                ('time', models.DateTimeField(auto_now_add=True)),
             ],
-            options={
-                'ordering': ['-created'],
-            },
         ),
     ]
