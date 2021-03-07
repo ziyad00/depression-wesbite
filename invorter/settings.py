2b524c3bbbdb36a563266a3add5e6560d12d6435
ziyad00 <ziyad.alotaibe@gmail.com>
2021-03-07 23:02:47 +0300

    dd

--- invorter/settings.py
+++ invorter/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
